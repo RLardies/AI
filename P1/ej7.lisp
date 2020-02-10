b042d99594e1f55eb91c1e4007040cb4f0b2482d
Lobo <carlitosg-99@hotmail.com>
2020-02-10 22:59:11 +0100

    ej7 terminado

--- P1/ej7.lisp
+++ P1/ej7.lisp
@@ -2,7 +2,8 @@
 	(backward-chaining-aux goal lst-rules NIL))
 
 (defun backward-chaining-aux (goal lst-rules pending-goals)
-	(if (member goal pending-goals) NIL 
+	(if (member goal pending-goals) NIL
 		(some #'(lambda (x) (if (equal (second x) goal) (if (null (car x)) t
-			(and (backward-chaining-aux (car (car x)) (remove x lst-rules) (cons goal pending-goals))
-				(backward-chaining-aux (second (car x)) (remove x lst-rules) (cons goal pending-goals)))))) lst-rules)))
+			(every #'identity (mapcar #'(lambda (y) 
+				(backward-chaining-aux y (remove x lst-rules) (cons goal pending-goals))) (car x))))))
+		lst-rules)))
