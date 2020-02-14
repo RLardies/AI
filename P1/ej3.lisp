d2f7928f8ca73132d239f4c3e24c9864fcf46c50
Lardis <lardiesrodrigo@gmail.com>
2020-02-14 11:26:28 +0100

    Ej6 añadido

--- P1/ej3.lisp
+++ P1/ej3.lisp
@@ -7,3 +7,12 @@
 
 (defun euclidean-distance (x y)
 	(euclidean-norm (mapcar #'- x y)))
+
+
+
+(equal (scalar-product '(1 2 3 4) '(1 1 1 1)) 10) ;; Test simple
+(equal (scalar-product '(1 2 3) '(3 2 1)) 10);; Test Simple
+(equal (euclidean-norm '(1 2 3)) (sqrt 14)) ;; Test Simple
+(equal (euclidean-norm '(-1 2 -5)) (sqrt 30)) ;; Test Simple
+(equal (euclidean-distance '(1 2 3) '(2 4 -8)) ) ;; Test Simple 
+(equal (euclidean-distance '(1 1 2 -1) '(-1 3 -1 1)) ) ;; Test Simple
