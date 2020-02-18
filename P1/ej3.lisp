d20b3716c9f3506b11dcb0c48443181a2b1e2e3a
Carlos Gómez-Lobo Hernaiz <carlos.gomez-lobo@estudiante.uam.es>
2020-02-18 15:05:24 +0100

    Añadido principal y modificados 3 y 4

--- P1/ej3.lisp
+++ P1/ej3.lisp
@@ -1,23 +1,23 @@
 (defun scalar-product (x y)
 	(apply #'+ (mapcar #'* x y)))
 
-;;Aquí se podría hacer con la anterior pero creo uqe así es más eficiente, preguntar
 (defun euclidean-norm (x)
 	(sqrt (apply #'+ (mapcar #'(lambda (x0) (* x0 x0)) x))))
 
 (defun euclidean-distance (x y)
 	(euclidean-norm (mapcar #'- x y)))
 
+;;Tests
 
+;; Apartado 1
+(= (scalar-product '(1 2 3 4) '(1 1 2 1)) 13) ;; Test simple
+(= (scalar-product '(1 2 3) '(0 0 0)) 0);; Test con vector 0
 
-(equal (scalar-product '(1 2 3 4) '(1 1 2 1)) 13) ;; Test simple
-(equal (scalar-product '(1 2 3) '(0 0 0)) 0);; Test Simple
+;; Apartado 2
+(= (euclidean-norm '(-1 2 -5)) (sqrt 30)) ;; Test Simple
+(= (euclidean-norm '(0 0 0)) (sqrt 0)) ;; Test con vector 0
 
-
-(equal (euclidean-norm '(-1 2 -5)) (sqrt 30)) ;; Test Simple
-(equal (euclidean-norm '(0 0 0)) (sqrt 0)) ;; Test Simple
-
-
-(equal (euclidean-distance '(1 2 3) '(2 4 -8)) (sqrt 126)) ;; Test Simple 
-(equal (euclidean-distance '(1 1 1) '(1 1 1)) 0) ;; Test Simple
-(equal (euclidean-distance '(1 1 1) '(1 2 3)) (sqrt 5)) ;; Test Simple
+;; Apartado 3
+(= (euclidean-distance '(1 2 3) '(2 4 -8)) (sqrt 126)) ;; Test Simple 
+(= (euclidean-distance '(1 1 1) '(1 1 1)) 0) ;; Test con vectores iguales
+(= (euclidean-distance '(1 1 1) '(0 0 0)) (sqrt 3)) ;; Test con vector 0
--- P1/ej4.lisp
+++ P1/ej4.lisp
@@ -1,27 +1,23 @@
-
-(defun cosine-similarity (x y) ;; Preguntar si debería comprobar los tamaños
+(defun cosine-similarity (x y)
 	(let ((scal-product (scalar-product x y)) 
 		(norm-product (* (euclidean-norm x) (euclidean-norm y))))
-		(if (/= 0 norm-product) ;"Si el producto de las normas es 0, devolver NIL"
+		(if (/= 0 norm-product) ;; Si el producto de las normas es 0, devolver NIL
 			(/ scal-product norm-product ))))
 
 (defun angular-distance (x y) 
-	(/ (acos (cosine-similarity x y)) pi))
+	(let ((cos-sim (cosine-similarity x y)))
+    (if (null cos-sim)
+      NIL
+      (/ (acos cos-sim) pi))))
 
 ;; Tests
 
 ;; Apartado 1
-(let (( res (cosine-similarity '(1 2 3) '(1 2 3))))
-	(and (> res (- 1 0.00001)) (< res (+ 1 0.00001)))) ;; Test vectores iguales
-(let (( res (cosine-similarity '(1 2 3) '(-4.5 7 0))))
-	(and (> res (- 1 0.00001)) (< res (+ 1 0.00001)))) ;; Test simple
-;;(let (( res (cosine-similarity '(0 0 0) '(1 2 3))))
-	;;(and (> res (- 1 0.00001)) (< res (+ 1 0.00001)))) No deja asignar NIL a la variable res
+(< (abs (- (cosine-similarity '(1 2 3) '(1 2 3)) 1)) 1.0L-6) ;; Test vectores iguales
+(< (abs (- (cosine-similarity '(1 2 3) '(-4.5 7 0)) 0.3051052626)) 1.0L-6) ;; Test simple
+(null (cosine-similarity '(0 0 0) '(1 2 3))) ;; Test con vector 0
 
 ;; Apartado 2
-(let (( res (angular-distance '(1 2 3) '(1 2 3))))
-	(and (> res (- 1 0.00001)) (< res (+ 1 0.00001)))) ;; Test vectores iguales
-(let (( res (angular-distance '(1 2 3) '(-4.5 7 0))))
-	(and (> res (- 1 0.00001)) (< res (+ 1 0.00001)))) ;; Test simple
-(let (( res (angular-distance '(0 0 0) '(1 2 3))))
-	(and (> res (- 1 0.00001)) (< res (+ 1 0.00001)))) ;; Test con vector 0
+(< (abs (- (angular-distance '(1 2 3) '(1 2 3)) 0)) 1.0L-3) ;; Test vectores iguales
+(< (abs (- (angular-distance '(1 2 3) '(-4.5 7 0)) 0.4013083507)) 1.0L-3) ;; Test simple
+(null (angular-distance '(0 0 0) '(1 2 3))) ;; Test con vector 0
