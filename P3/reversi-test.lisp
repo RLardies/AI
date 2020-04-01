df50da5b09be88b225c6efbb3336b105924c0325
Carlos Gómez-Lobo Hernaiz <carlos.gomez-lobo@estudiante.uam.es>
2020-04-01 21:31:07 +0200

    Weight-mobility añadida

--- P3/reversi-test.lisp
+++ P3/reversi-test.lisp
@@ -35,34 +35,6 @@
   (apply #'max (mapcar #'(lambda (row) (count player row)) brd))))
 
 (defun weight-fn (player board)
-  (let ((brd board));(get-board board)))
-    (apply #'+ (mapcar #'(lambda (boardrow weightrow) 
-        (apply #'+ (mapcar #'(lambda (brd-player square-weight)
-          (cond 
-            ((= player brd-player)
-              square-weight)
-            ((= player (opponent player))
-              (- square-weight))
-            (t
-              0)))
-          boardrow weightrow)))
-    brd *pesos*))))
-
-;; (reversi #'human (alpha-beta-searcher 2 #'mobility))
-
-(round-robin
- (list (alpha-beta-searcher 2 #'count-difference)
-       (alpha-beta-searcher 2 #'mobility)
-       (alpha-beta-searcher 2 #'weight-fn)
-       #'random-strategy)
- 20
- 10
- '(count-difference
-   mobility
-   weight-fn
-   random-strategy))
-
-(defun weight-fn (player board)
   (let ((brd (get-board board)))
     (apply #'+ 
       (mapcar 
@@ -84,12 +56,40 @@
     (t
       0)))
 
-(defparameter *boardtest*
-  (list '(1 0 2)
-    '(0 1 0)
-    '(2 0 1)))
+(defun game-status (player board)
+  (let ((brd (get-board board)))
+    (apply #'+ (mapcar #'(lambda (row)
+      (apply #'+ (mapcar #'(lambda (square)
+        (if (or (= square player) (= square (opponent player))) 1 0)) row))) brd))))
 
-(defparameter *pesostest*
-  (list (2 3 -2)
-    (1 -2 5)
-    (0 3 -5)))
+(defun weight-mobility (player board)
+  (let ((status (game-status player board))
+        (mobility-value (mobility player board))
+        (weight-value (weight player board)))
+    (+ (/ (* status weight-value) 64) (/ (* (- 64 status) mobility-value) 64))))
+
+;; (reversi #'human (alpha-beta-searcher 2 #'mobility))
+
+(round-robin
+ (list (alpha-beta-searcher 2 #'count-difference)
+       (alpha-beta-searcher 2 #'mobility)
+       (alpha-beta-searcher 2 #'weight-fn)
+       (alpha-beta-searcher 2 #'weight-mobility)
+       #'random-strategy)
+ 20
+ 10
+ '(count-difference
+   mobility
+   weight-fn
+   weight-mobility
+   random-strategy))
+
+(defparameter *pesos* 
+  '((100 -20 10 5 5 10 -20 100)
+    (-20 -50 -2 -2 -2 -2 -50 -20)
+    (10 -2 -1 -1 -1 -1 -2 10)
+    (5 -2 -1 -1 -1 -1 -2 5)
+    (5 -2 -1 -1 -1 -1 -2 5)
+    (10 -2 -1 -1 -1 -1 -2 10)
+    (-20 -50 -2 -2 -2 -2 -50 -20)
+    (100 -20 10 5 5 10 -20 100)))
